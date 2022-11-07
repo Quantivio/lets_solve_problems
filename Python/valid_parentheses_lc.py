970e6e8856aeccf10c57885ffae3e0755f848136
Vetrichelvan <pythonhub.py@gmail.com>
2022-11-07 23:42:25 +0530

    Added description for valid parentheses problem

--- Python/valid_parentheses_lc.py
+++ Python/valid_parentheses_lc.py
@@ -1,22 +1,32 @@
-from time import perf_counter
+# Leet Code Problem: Valid Parentheses
+# Link: https://leetcode.com/problems/valid-parentheses/
+# Complexity: Easy
+# Description: Determine whether given set of parentheses is valid.
+"""
+You may assume that the integer is not negative.
+Example:
+    Input: {[]}
+    Output: true
+    Because {[]} is a valid set of parentheses because each set of parentheses close each other.
+"""
 
-PARENTHESES_MAPPING = {
-    "(": ")",
-    "{": "}",
-    "[": "]",
-    "<": ">",
-}
+from time import perf_counter
 
 
 class ValidParentheses:
     @staticmethod
     def valid_parentheses(string: str) -> bool:
         bracket_list = []
-        bracket_mapping = dict(zip('([{', ')]}'))
+        bracket_mappings = {
+            "(": ")",
+            "{": "}",
+            "[": "]",
+            "<": ">",
+        }
         for char in string:
             if char in "({[":  # change to '([{' for better performance
                 bracket_list.append(char)
-            elif not bracket_list or bracket_mapping[bracket_list.pop()] != char:
+            elif not bracket_list or bracket_mappings[bracket_list.pop()] != char:
                 return False
 
         return not bracket_list
